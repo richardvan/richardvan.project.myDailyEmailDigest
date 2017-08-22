d9a733936824a7458a15cd1dfa57934bb2d9fb26
Richard Van <richardvan@Richards-Mac-mini.local>
2017-08-22 14:17:16 -0700

    added past entries

--- scheduled-jobs-greVocab.js
+++ scheduled-jobs-greVocab.js
@@ -36,6 +36,33 @@ var myGREwordDictionary = {
 	'amalgamate'		:   "mix; combine; unite societies"
 }
 
+var myGREwordsSeenMOCK = {
+	// all words that have been send over the wire naturally at 7:30am as of 8/22/2017
+	'adorn'				:   "add beauty; decorate",		// 8/22
+	'adorn2'				:   "add beauty twice"		// test
+}
+
+
+//////////////////////////////////////
+/////////
+///////.    Network Calls
+/////////
+//////////////////////////////////////
+
+function getFromMongo_GREwordsSeen(){ 
+
+	console.log ("  +++++++> calling getFromMongo_GREwordsSeen()");
+	// connect to mongobd
+
+	var returnValue = '';
+
+	// make call to get most recent dictionary of greWordsSent so far
+	console.log ("  -------> does nothing for now ");
+
+
+	console.log ("  +++++++> end getFromMongo_GREwordsSeen()");
+
+}
 
 /**
  * Returns a random integer between min (inclusive) and max (inclusive)
@@ -43,6 +70,48 @@ var myGREwordDictionary = {
  */
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+function getPastGREwords(){
+
+	console.log ("  ------> calling getPastGREwords()");
+	
+	var returnValue = myGREwordsSeenMOCK;
+	// var returnValue = myGREwordsSeenNETWORK;
+	// dictGREwordsSeen = myGREwordsSeenNETWORK
+
+	console.log ("  ------> return size: %i", Object.keys(returnValue).length);
+
+	return returnValue;
+}
+
+
+function getPastGREwordsContent(dict){
+
+	console.log ("  ------> calling getPastGREwordscontent()");
+	
+	var returnValue = "";
+
+	console.log ("  ------>    dict: %s", JSON.stringify(dict));
+	console.log ("  ------>    dict size: %i", Object.keys(dict).length);
+
+
+	for (var key in dict) {
+	    if (dict.hasOwnProperty(key)) {
+	        console.log(key, dict[key]);
+			console.log("  ------>    %s,%s", key, dict[key]);
+
+			returnValue = returnValue + '<b>' + key +'</b>: ' + dict[key] + '<br>';
+	    }
+	}
+
+
+	console.log ("  ------>    returnValue: %s", Object.keys(dict).length);
+
+	console.log ("  ------> end getPastGREwordscontent");
+
+	return returnValue;
 }
 
 function getNextGREword(){
@@ -61,30 +130,48 @@ function getNextGREword(){
 function sendEmail(){
 	console.log ("  ----> calling sendEmail()");
 
+	var pastGREwords = getPastGREwords();
+	var pastGREwordsContent = getPastGREwordsContent(pastGREwords);
 	var currentGREword = getNextGREword();
 
 	var helper = require('sendgrid').mail;
-	var from_email = new helper.Email('testing hehe <herokuDailyEmailDigest@shrouded-brook.com>');
+	var from_email = new helper.Email('greVocabMockup@hongboDailyEmailDigest._todoUpdateThisCounter');
 	var to_email = new helper.Email('vanrichard@gmail.com');
 	var subject = '[#GREvocabulary] ' + currentGREword;
 	console.log ("  ------> subject: %s", subject);
 
-	var contentString = 	'Hello Hong,<br>' + 
+	var contentString = 	'Hello Hongbo,<br>' + 
 							'<br>' +
 							'<b>GRE Word of the DAY</b><br>' +
-							'<b>' + currentGREword +'</b>: ' + myGREwordDictionary[currentGREword] + 
-							'<br>' +
+							'<b>' + currentGREword +'</b>: ' + myGREwordDictionary[currentGREword] + '<br>' +
 							'<br>' +
 							'<br>' +
 							'Other stuff to remember<br>' +
-							'&ensp;&ensp;&ensp;&ensp;&#9634;     check out rosalind.info (add in link)' + 
-							'&ensp;&ensp;&ensp;&ensp;&#9634;     do another ochem module (end this on __todo_check final date' + 
+							'&ensp;&ensp;&ensp;&ensp;&#9634;     check out rosalind.info (add in link) <br>' + 
+							'&ensp;&ensp;&ensp;&ensp;&#9634;     do another ochem module (end this on __todo_check final date <br>' + 
 							'&ensp;&ensp;&ensp;&ensp;&#9634;     chinese learning (todo_add my name)<br>' +
 							'&ensp;&ensp;&ensp;&ensp;&#9634;     breath horizontally - (todo_add more pictures, details, sheet)<br>' +
 							'&ensp;&ensp;&ensp;&ensp;&#9634;     daily quote - (todo_add similiar dictionary to grevocab)<br>' +
 							'&ensp;&ensp;&ensp;&ensp;&#9634;     read<br>' +
 							'&ensp;&ensp;&ensp;&ensp;&#9634;     chinese learning (todo_add my name)<br>' +
+							'<br>' +
+							'<br>' +
+							'<br>' +
+							'<br>' +
+							'<b>GRE Words seen thus far</b><br>' +
+							'<h4>List of past words sent thus far!</h4><br>: ' + 
+							pastGREwordsContent +  '<br>' + 
+							'_____need________connectTOcommonDATABASE__<br>' +
+							'_____need________parser to display this list nice html format__<br>' +
+							'<br>' +
+							'<br>' +
+							'[ end of message hongbo ]' +
+
+
+
 							'';
+
+
 
 	var content = new helper.Content('text/html', contentString);
 	console.log ("  ------> contentString: %s", contentString);
@@ -108,5 +195,22 @@ function sendEmail(){
 
 	console.log ("  ----> end sendEmail() function");
 }
-getNextGREword();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// getNextGREword();
 sendEmail();
