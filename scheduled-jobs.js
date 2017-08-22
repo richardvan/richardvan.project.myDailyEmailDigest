efafd6f75da3f6f931285827ccbfef54d6460cd8
Richard Van <richardvan@Richards-Mac-mini.local>
2017-08-21 21:44:48 -0700

    adding in scheduled jobs for heroku scheduler

---
+++ scheduled-jobs.js
@@ -0,0 +1,9 @@
+// initially from http://www.modeo.co/blog/2015/1/8/heroku-scheduler-with-nodejs-tutorial
+
+function sayHello() {
+    console.log('Hello 1');
+    console.log('Hello 2');
+    console.log('Hello 3');
+    console.log('Hello 4');
+}
+sayHello();
---
+++ views/scheduled-job.js
@@ -0,0 +1,6 @@
+// from http://www.modeo.co/blog/2015/1/8/heroku-scheduler-with-nodejs-tutorial
+
+function sayHello() {
+    console.log('Hello');
+}
+sayHello();
